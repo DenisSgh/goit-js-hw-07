eecfa9423f4cc092a56e7bb5b80047790b703ace
Denis Singh <denis.darkrgd@gmail.com>
2021-04-09 19:00:52 +0300

    Complete 8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,10 +1,53 @@
-//* Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит количество элементов в input и нажимает кнопку Создать, после чего рендерится коллекция. При нажатии на кнопку Очистить, коллекция элементов очищается.
+const refs = {
+  inputRef: document.querySelector('#controls input'),
+  buttonCreate: document.querySelector(
+    '#controls button[data-action="render"]',
+  ),
+  buttonClear: document.querySelector(
+    '#controls button[data-action="destroy"]',
+  ),
+  boxesRef: document.querySelector('#boxes'),
+};
 
-//* Создай функцию createBoxes(amount), которая принимает 1 параметр amount - число. Функция создает столько div, сколько указано в amount и добавляет их в div#boxes.
+refs.buttonCreate.addEventListener('click', appendBoxes);
+refs.buttonClear.addEventListener('click', clearBoxes);
 
-//* Каждый созданный div:
+function appendBoxes() {
+  clearBoxes();
+  createBoxes(refs.inputRef.value);
+}
 
-//* Имеет случайный rgb цвет фона
-//* Размеры самого первого div - 30px на 30px
-//* Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
-//* Создай функцию destroyBoxes(), которая очищает div#boxes.
+function clearBoxes() {
+  refs.boxesRef.innerHTML = '';
+}
+
+function createBoxes(amount) {
+  let divSize = { width: 30, height: 30 };
+  const items = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    const randomColor = getRandomRgb();
+    const divElement = createDivBoxes(
+      divSize.width,
+      divSize.height,
+      randomColor,
+    );
+    items.push(divElement);
+    divSize.width += 10;
+    divSize.height += 10;
+  }
+
+  refs.boxesRef.append(...items);
+}
+
+function createDivBoxes(width, height, color) {
+  const createBox = document.createElement('div');
+  createBox.style.width = `${width}px`;
+  createBox.style.height = `${height}px`;
+  createBox.style.backgroundColor = color;
+
+  return createBox;
+}
+
+const getRandomRgb = () =>
+  '#' + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase();
